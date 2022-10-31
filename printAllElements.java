ec3a5f7432fbdd9096a14c81df86f10a5bb284a1
Sunit Ajgar <109612599+SunitAjgar@users.noreply.github.com>
2022-11-01 02:27:47 +0530

    Program to print the elements of an array

---
+++ printAllElements.java
@@ -0,0 +1,18 @@
+package com.blz.arraysDay3;
+
+import java.util.Scanner;
+
+public class printAllElements {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int arr[] = new int[10];
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println("Enter the element");
+			arr[i] = sc.nextInt();
+		}
+		System.out.print("The array is ");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+}
