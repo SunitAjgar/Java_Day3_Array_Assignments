735a25ca66d90a4928edb95e076269a840427206
Sunit Ajgar <109612599+SunitAjgar@users.noreply.github.com>
2022-11-02 19:46:05 +0530

    printing largest element in array

--- largestElement.java
+++ largestElement.java
@@ -10,7 +10,7 @@ public class largestElement {
 		int[] arr = new int[size];
 		for (int i = 0; i < arr.length; i++) {
 		System.out.println("Enter the elements");
-        arr[i] = sc.nextInt();
+                arr[i] = sc.nextInt();
 		}
 		int max = arr[0];
 		for(int i = 1; i < arr.length; i++) {
