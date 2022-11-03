9630541d48d4ecc60a6f46ed4bc823854874090d
Sunit Ajgar <109612599+SunitAjgar@users.noreply.github.com>
2022-11-03 12:50:27 +0530

    Program to sort the elements of an array in ascending order

--- printAscending.java
+++ printAscending.java
@@ -22,9 +22,9 @@ public class printAscending {
 		for (int i = 0; i < arr.length; i++) {
 		for (int j = i+1; j < arr.length; j++) {
 		if (arr[i] > arr[j]) {
-	    hold = arr[i];
+	        hold = arr[i];
 		arr[i] = arr[j];
-	    arr[j] = hold;
+	        arr[j] = hold;
 				}
 			}
 		}
