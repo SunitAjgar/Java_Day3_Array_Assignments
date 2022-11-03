6436763f349242abe2f3e68cd6a93ac2962f018f
Sunit Ajgar <109612599+SunitAjgar@users.noreply.github.com>
2022-11-04 01:26:29 +0530

    Program to print duplicate elements in the array

--- DuplicateElement.java
+++ DuplicateElement.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 public class DuplicateElement {  
     public static void main(String[] args) {      
             
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the size of the array");
-		int size = sc.nextInt();
-		int[] arr = new int[size];
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter the size of the array");
+	int size = sc.nextInt();
+	int[] arr = new int[size];
 
-		for (int i = 0; i < arr.length; i++) {
-		System.out.print("Enter the element at index " + i + " " + "is : ");
-		arr[i] = sc.nextInt();
-		}
-	    System.out.print("The original array is : ");
-	    for(int i = 0; i < arr.length; i++) {
-	    	System.out.print(arr[i] + " ");
-	    }
+	for (int i = 0; i < arr.length; i++) {
+	System.out.print("Enter the element at index " + i + " " + "is : ");
+	arr[i] = sc.nextInt();
+	}
+	System.out.print("The original array is : ");
+	for(int i = 0; i < arr.length; i++) {
+	System.out.print(arr[i] + " ");
+	}
         System.out.println();  
         System.out.print("Duplicate elements in given array: ");  
         for(int i = 0; i < arr.length; i++) {  
